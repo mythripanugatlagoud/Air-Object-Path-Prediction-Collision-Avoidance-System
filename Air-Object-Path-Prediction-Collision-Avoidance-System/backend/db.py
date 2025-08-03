f5484dc267da3f8881a95fce4ed1ec76f25b3cf6
Mythri panugatla <mp9816@srmist.edu.in>
2025-08-03 17:51:29 +0530

    Update db.py

--- Air-Object-Path-Prediction-Collision-Avoidance-System/backend/db.py
+++ Air-Object-Path-Prediction-Collision-Avoidance-System/backend/db.py
@@ -1,12 +1,13 @@
 import psycopg2
 
 def get_connection():
-    return psycopg2.connect(
+    return psycopg2.connect(   
         dbname="drdlproject",      
         user="postgres",                 
         password="drdl123",         
         host="localhost",           # your PC
         port="5432"                 # default PostgreSQL port   
     )
+
 
 
