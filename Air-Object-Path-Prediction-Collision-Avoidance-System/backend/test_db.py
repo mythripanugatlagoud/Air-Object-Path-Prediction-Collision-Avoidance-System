47ef22c3c9a8589b9937e48e5570de33bba731b1
Mythri panugatla <mp9816@srmist.edu.in>
2025-09-06 21:37:52 +0530

    Update test_db.py

--- Air-Object-Path-Prediction-Collision-Avoidance-System/backend/test_db.py
+++ Air-Object-Path-Prediction-Collision-Avoidance-System/backend/test_db.py
@@ -1,8 +1,9 @@
 from db import get_connection
-
+ 
 try:
     conn = get_connection()
     print("Connected to PostgreSQL successfully!")
     conn.close()
 except Exception as e:
     print("Failed to connect:", e)
+
